3181fc71716be30ad91d8b554209a4f90e0a0ba3
Nicophoto04 <143752578+Pistol15@users.noreply.github.com>
2026-04-08 12:11:07 +0200

    Création de fichier séparer pour les css + Création de la page portfolio avce ces liens cliquable

--- app.py
+++ app.py
@@ -14,6 +14,22 @@ def menu():
 def portfolio():
     return render_template('portfolio/portfolio.html', title="portfolio")
 
+@app.route('/MotocrossBroc2025')
+def MotocrossBroc2025():
+    return render_template('portfolio/Motocross_de_Broc_2025.html', title="Motocross de Broc 2025")
+
+@app.route('/MotocrossTrey2025')
+def MotocrossTrey2025():
+    return render_template('portfolio/Motocross_de_Trey_2025.html', title="Motocros de Trey 2025")
+
+@app.route('/MotocrossCournillens2025')
+def MotocrossCournillens2025():
+    return render_template('portfolio/Motocross_de_Cournillens_2025.html', title="Motocros de Cournillens 2025")
+
+@app.route('/MotocrossSergey2025')
+def MotocrossSergey2025():
+    return render_template('portfolio/Motocross_de_Sergey_2025.html', title="Motocros de Sergey 2025")
+
 if __name__ == '__main__':
     # Mode debug activé pour le développement
     app.run(debug=True)
